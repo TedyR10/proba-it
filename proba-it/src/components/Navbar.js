75db900500c00158614ac3da9e403e87986e8f74
Theodor Rolea <theodor.rolea@stud.acs.upb.ro>
2021-11-24 00:48:34 +0200

    nav start

--- proba-it/src/components/Navbar.js
+++ proba-it/src/components/Navbar.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Bazaroo <i className="fab fa-typo3"></i>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
